84e4ec60fa5c01ed83574d50dcb8b158a139230a
Gavin <tianyu@hotmail.com>
2015-07-26 21:28:57 +0800

    a

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,7 @@
     <link href="{{ asset('/css/site.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" id="top">
+    <header class="navbar navbar-inverse navbar-fixed-top" id="top">
         <div class="container">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar" aria-expanded="false">
@@ -29,7 +29,7 @@
                 </button>
                 <a class="navbar-brand" href="#">FiredMushroom</a>
             </div>
-            <div class="collapse navbar-collapse" id="bs-navbar">
+            <nav class="collapse navbar-collapse" id="bs-navbar">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#">主页</a></li>
                     <li><a href="#">产品</a></li>
@@ -37,11 +37,11 @@
                     <li><a href="#">帮助</a></li>
                     <li><a href="#">登录</a></li>
                 </ul>
-            </div>
+            </nav>
         </div>
-    </div>
+    </header>
 
-    <div class="jumbotron masthead text-center masthead-bg-world">
+    <div class="jumbotron masthead text-center">
         <div class="container">
             <h1>联结世界</h1>
             <h2>给您一个真正的互联网</h2>
@@ -51,6 +51,124 @@
             </p>
         </div>
     </div>
+
+    <div class="container nav-service">
+        <h2>优质服务</h2>
+        <div class="row">
+            <div class="col-sm-6 col-md-4 col-lg-3 ">
+                <img src="{{asset('/img/icon_ser_1.png')}}" alt="" class="img-responsive">
+                <div class="caption">
+                    <h3>真诚贴心服务</h3>
+                    <p>
+                        安心保障，首次购买3天内全额退款
+                        新技术，新思维
+                        新特权，心服务
+                        快乐生活，放心消费
+                        自由无处不在!
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4 col-lg-3 ">
+                <img src="{{asset('/img/icon_ser_2.png')}}" alt="" class="img-responsive">
+                <div class="caption">
+                    <h3>多条快速海外线路</h3>
+                    <p>
+                        日本、美国、香港、台湾、英国、法国多条线路，稳定超流畅，上网如飞。
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4 col-lg-3 ">
+                <img src="{{asset('/img/icon_ser_3.png')}}" alt="" class="img-responsive">
+                <div class="caption">
+                    <h3>多条快速海外线路</h3>
+                    <p>
+                        日本、美国、香港、台湾、英国、法国多条线路，稳定超流畅，上网如飞。
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4 col-lg-3 ">
+                <img src="{{asset('/img/icon_ser_4.png')}}" alt="" class="img-responsive">
+                <div class="caption">
+                    <h3>全平台全设备</h3>
+                    <p>
+                        无需安装任何软件
+                        全平台支持，无论在电脑前还是移动
+                        中，都能畅游互联网
+                        只需敲几下键盘，点几下鼠标
+                        你的网络生活更自由更精彩！
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container">
+            <h2>价格</h2>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-lg-3 ">
+                    <table class="table table-bordered">
+                        <tr><td>基础包月版 - ￥15/月</td></tr>
+                        <tr><td>每月 10Gb 流量</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>可同时1台设备在线</td></tr>
+                        <tr><td><button type="button" class="btn btn-primary">订购</button></td></tr>
+                    </table>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 ">
+                    <table class="table table-bordered">
+                        <tr><td>基础包月版 - ￥15/月</td></tr>
+                        <tr><td>每月 10Gb 流量</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>可同时1台设备在线</td></tr>
+                        <tr><td><button type="button" class="btn btn-primary">订购</button></td></tr>
+                    </table>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 ">
+                    <table class="table table-bordered">
+                        <tr><td>基础包月版 - ￥15/月</td></tr>
+                        <tr><td>每月 10Gb 流量</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>可同时1台设备在线</td></tr>
+                        <tr><td><button type="button" class="btn btn-primary">订购</button></td></tr>
+                    </table>
+                </div>
+
+                <div class="col-sm-6 col-md-4 col-lg-3 ">
+                    <table class="table table-bordered">
+                        <tr><td>基础包月版 - ￥15/月</td></tr>
+                        <tr><td>每月 10Gb 流量</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>线路任意切换</td></tr>
+                        <tr><td>可同时1台设备在线</td></tr>
+                        <tr><td><button type="button" class="btn btn-primary">订购</button></td></tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2>联系我们</h2>
+            <h6>咨询,建议都可以联系我们</h6>
+        </div>
+
+        <footer>
+            <div class="container">
+                <ul class="list-inline text-center">
+                    <li><a href="#">©2015 isgavin.me 备案号:津ICP备15004268号-1</a></li>
+                </ul>
+            </div>
+        </footer>
+
+    </div>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
